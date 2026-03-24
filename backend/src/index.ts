d1e754af8ec89fcac182387a5c525f424bc879a6
Kubra Korkmaz <99243012+kybrakorkmaz@users.noreply.github.com>
2026-03-24 04:06:50 +0300

    implement GET all subjects

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
+import express from "express";
+import subjectsRouter from "./routes/subjects"
+import cors from "cors";
+const app = express();
+const PORT = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
+app.use('/', (req, res)=>{
+    res.send('Hello, welcome to API')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}}`);
+})
